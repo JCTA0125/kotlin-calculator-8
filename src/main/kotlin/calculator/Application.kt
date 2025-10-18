57bff25657be60da2912dfb9593936344716e6fd
JCTA0125 <jcta0125@duksung.ac.kr>
2025-10-18 20:20:47 +0900

    feat: 출력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,10 +3,16 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    getInput()
+    val input = getInput()
+    printResult(input)
 }
 
-private fun getInput() {
+private fun getInput(): String {
     println("덧셈할 문자열을 입력해주세요.")
     val input = Console.readLine()
+    return input
+}
+
+private fun printResult(result: String) {
+    println(result)
 }
