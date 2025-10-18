7aa15aa86b941f3a60c11a0834cc0f9c7a5918c2
JCTA0125 <jcta0125@duksung.ac.kr>
2025-10-18 21:46:03 +0900

    feat: 커스텀 구분자 여부 확인

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     val input = getInput()
+    checkCustom(input)
     printResult(input)
 }
 
@@ -11,6 +12,24 @@ private fun getInput(): String {
     println("덧셈할 문자열을 입력해주세요.")
     val input = Console.readLine()
     return input
+}
+
+private fun checkCustom(input: String) {
+    val customStart = input.indexOf("//")
+    val customEnd = input.indexOf("\\n")
+    if (customStart == 0 && customEnd == 3){
+        // Custom Calculator
+        // TODO: Custom Calculator
+    } else if (customStart != 0 && customEnd == 3) {
+        // Custom Separator \\(X), \n(O)
+        throw IllegalArgumentException("customStart: $customStart")
+    } else if (customStart == 0 && customEnd != 3){
+        // Custom Separator \\(O), \n(X)
+        throw IllegalArgumentException("customEnd: $customEnd")
+    } else {
+        // Basic Calculator
+        // TODO: Basic Calculator
+    }
 }
 
 private fun printResult(result: String) {
